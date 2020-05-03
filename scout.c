d0d617f19621dda746414de398b8b5e8d8d9de83
cdetrio <cdetrio@gmail.com>
2020-05-03 18:48:40 -0400

    fix inv, some param orders

--- scout.c
+++ scout.c
@@ -73,7 +73,7 @@ void eth2_savePostStateRoot(wasm_exec_env_t exec_env, uint8_t* mem){
 //modinv = 0x14fec701e8fb0ce9ed5e64273c4f538b1797ab1458a88de9343ea97914956dc87fe11274d898fafbf4d38259380b48
 //rsquared = 0x11988fe592cae3aa9a793e85b519952d67eb88a9939d83c08de5476c4c95b6d50a76e6a609d104f1f4df1f341c341746
 uint64_t mod[] = {0xb9feffffffffaaab, 0x1eabfffeb153ffff, 0x6730d2a0f6b0f624, 0x64774b84f38512bf, 0x4b1ba7b6434bacd7, 0x1a0111ea397fe69a};
-uint64_t modinv[] = {0xf4d38259380b4820, 0x7fe11274d898fafb, 0x343ea97914956dc8, 0x1797ab1458a88de9, 0xed5e64273c4f538b, 0x14fec701e8fb0ce9};
+uint64_t modinv[] = {0x89f3fffcfffcfffd};
 uint64_t rsquared[] = {0xf4df1f341c341746, 0xa76e6a609d104f1, 0x8de5476c4c95b6d5, 0x67eb88a9939d83c0, 0x9a793e85b519952d, 0x11988fe592cae3aa};
 
 // bigint functions come from bigint.h
@@ -90,16 +90,6 @@ void debug_printMemHex(wasm_exec_env_t exec_env, uint8_t* mem, uint32_t length){
   if(verbose) printf("eth2_debugPrintMem(%p, %u)\n", mem, length);
 }
 
-void bignum_f1m_toMontgomery(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x){
-  if(verbose) printf("bignum_f1m_toMontgomery()\n");
-  //FUNCNAME(montmul)((UINT*)out,(UINT*)x,(UINT*)rsquared,(UINT*)mod,((UINT*)modinv)[0]);
-}
-
-void bignum_f1m_fromMontgomery(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x){
-  if(verbose) printf("bignum_f1m_fromMontgomery()\n");
-  //FUNCNAME(montreduce)((UINT*)out,(UINT*)x,(UINT*)mod,((UINT*)modinv)[0]);
-}
-
 int f1m_mul_counter=0;
 void bignum_f1m_mul(wasm_exec_env_t exec_env, uint8_t* a1, uint8_t* a2, uint8_t* a3){
   if(verbose) printf("bignum_f1m_mul()\n");
@@ -107,39 +97,40 @@ void bignum_f1m_mul(wasm_exec_env_t exec_env, uint8_t* a1, uint8_t* a2, uint8_t*
   FUNCNAME(montmul)((UINT*)a3,(UINT*)a2,(UINT*)a1,(UINT*)mod,modinv[0]);
 }
 
-void bignum_f1m_add(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+//void bignum_f1m_add(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+void bignum_f1m_add(wasm_exec_env_t exec_env, uint8_t* x, uint8_t* y, uint8_t* out){
   if(verbose) printf("bignum_f1m_add()\n");
-  //FUNCNAME(addmod)((UINT*)out,(UINT*)x,(UINT*)y,(UINT*)mod);
+  FUNCNAME(addmod)((UINT*)out,(UINT*)x,(UINT*)y,(UINT*)mod);
 }
 
-void bignum_f1m_sub(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+//void bignum_f1m_sub(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+void bignum_f1m_sub(wasm_exec_env_t exec_env, uint8_t* x, uint8_t* y, uint8_t* out){
   if(verbose) printf("bignum_f1m_sub()\n");
-  //FUNCNAME(subtractmod)((UINT*)out,(UINT*)x,(UINT*)y,(UINT*)mod);
+  FUNCNAME(subtractmod)((UINT*)out,(UINT*)x,(UINT*)y,(UINT*)mod);
 }
 
-void bignum_f1m_square(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x){
-  if(verbose) printf("bignum_f1m_square()\n");
-  //FUNCNAME(montsquare)((UINT*)out,(UINT*)x,(UINT*)mod,modinv[0]);
-}
-
-void bignum_int_mul(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+//void bignum_int_mul(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+void bignum_int_mul(wasm_exec_env_t exec_env, uint8_t* x, uint8_t* y, uint8_t* out){
   if(verbose) printf("bignum_int_mul()\n");
-  //FUNCNAME(mul)((UINT*)out,(UINT*)x,(UINT*)y);
+  FUNCNAME(mul)((UINT*)out,(UINT*)x,(UINT*)y);
 }
 
-uint32_t bignum_int_add(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+//uint32_t bignum_int_add(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+uint32_t bignum_int_add(wasm_exec_env_t exec_env, uint8_t* x, uint8_t* y, uint8_t* out){
   if(verbose) printf("bignum_int_add()\n");
-  //return FUNCNAME(add)((UINT*)out,(UINT*)x,(UINT*)y);
+  return FUNCNAME(add)((UINT*)out,(UINT*)x,(UINT*)y);
 }
 
-uint32_t bignum_int_sub(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+//uint32_t bignum_int_sub(wasm_exec_env_t exec_env, uint8_t* out, uint8_t* x, uint8_t* y){
+uint32_t bignum_int_sub(wasm_exec_env_t exec_env, uint8_t* x, uint8_t* y, uint8_t* out){
   if(verbose) printf("bignum_int_sub()\n");
-  //return FUNCNAME(subtract)((UINT*)out,(UINT*)x,(UINT*)y);
+  return FUNCNAME(subtract)((UINT*)out,(UINT*)x,(UINT*)y);
 }
 
-void bignum_int_div(wasm_exec_env_t exec_env, uint8_t* remainder, uint8_t* quotient, uint8_t* x, uint8_t* y){
+//void bignum_int_div(wasm_exec_env_t exec_env, uint8_t* remainder, uint8_t* quotient, uint8_t* x, uint8_t* y){
+void bignum_int_div(wasm_exec_env_t exec_env, uint8_t* x, uint8_t* y, uint8_t* quotient, uint8_t* remainder){
   if(verbose) printf("bignum_int_div()\n");
-  //FUNCNAME(div)((UINT*)quotient,(UINT*)remainder,(UINT*)x,(UINT*)y);
+  FUNCNAME(div)((UINT*)quotient,(UINT*)remainder,(UINT*)x,(UINT*)y);
 }
 
 #undef BIGINT_BITS
@@ -157,12 +148,9 @@ static NativeSymbol native_symbols[] = {
   EXPORT_WASM_API_WITH_SIG(eth2_savePostStateRoot,"(*)"),
   EXPORT_WASM_API_WITH_SIG(eth2_pushNewDeposit,"(*~)"),
   EXPORT_WASM_API_WITH_SIG(debug_printMemHex,"(*~)"),
-  EXPORT_WASM_API_WITH_SIG(bignum_f1m_toMontgomery,"(**)"),
-  EXPORT_WASM_API_WITH_SIG(bignum_f1m_fromMontgomery,"(**)"),
   EXPORT_WASM_API_WITH_SIG(bignum_f1m_mul,"(***)"),
   EXPORT_WASM_API_WITH_SIG(bignum_f1m_add,"(***)"),
   EXPORT_WASM_API_WITH_SIG(bignum_f1m_sub,"(***)"),
-  EXPORT_WASM_API_WITH_SIG(bignum_f1m_square,"(**)"),
   EXPORT_WASM_API_WITH_SIG(bignum_int_mul,"(***)"),
   EXPORT_WASM_API_WITH_SIG(bignum_int_add,"(***)i"),
   EXPORT_WASM_API_WITH_SIG(bignum_int_sub,"(***)i"),
